8ba2414fd3657a0e27c778bc4735f61de98f7854
Nick <nicnowak@nmu.edu>
2019-02-21 18:55:16 -0500

    recursion passes VIII

--- Converter.js
+++ Converter.js
@@ -1,19 +1,23 @@
 var Converter = {
     m: {1000: "M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 4:"IV", 1: "I"},
-    
+
     //arabic to roman 
     a2r: function(a, ret=""){
-        if (this.m[a] != undefined){
-            ret += this.m[a];
-            return ret;
+        var self = this;
+
+        if (self.m[a] != undefined){
+            return ret += this.m[a];
         }
         
-        Object.keys(this.m).forEach(function(key) {
+        var keys = Object.keys(this.m);
+        
+        for (var i = keys.length; i >= 0; i--){
+            var key = keys[i]
             if (a > key){
-                ret += this.m[key]
-                if (a-key > 1) return this.a2r(a-key, ret);
+                ret += self.m[key];
+                return self.a2r(a-key, ret)
             }
-        });
+        }
         
         return ret;
     }
