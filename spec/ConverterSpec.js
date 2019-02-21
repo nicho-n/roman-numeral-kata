34810b7fac76d9a6077bfef09220739766d86a8a
nick <nicholaspnowak@gmail.com>
2019-02-20 23:07:17 -0500

    tab formatting

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -2,7 +2,7 @@ var Converter = require("../Converter");
 
 describe("Arabic to Roman Test cases", function() {
 
-	it("converter should exist", function() {
+    it("converter should exist", function() {
 		expect(Converter).not.toEqual(undefined);
     });
     
