5b2932ee4a45860e2a71b27ac7c4621a00869aa8
Nick <nicnowak@nmu.edu>
2019-02-21 18:27:50 -0500

    recursive solution fails

--- Converter.js
+++ Converter.js
@@ -1,12 +1,20 @@
 var Converter = {
-    m: {1000: "M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 1:"1"},
-    ret: "",
+    m: {1000: "M", 500:"D", 100:"C", 50:"L", 10:"X", 5:"V", 4:"IV", 1: "I"},
     
     //arabic to roman 
-    a2r: function(a){
-        for (var key in m){
-            if (a == key) return key;
+    a2r: function(a, ret=""){
+        if (this.m[a] != undefined){
+            ret += this.m[a];
+            return ret;
         }
+        
+        Object.keys(this.m).forEach(function(key) {
+            if (a > key){
+                ret += this.m[key]
+                if (a-key > 1) return this.a2r(a-key, ret);
+            }
+        });
+        
         return ret;
     }
 }
